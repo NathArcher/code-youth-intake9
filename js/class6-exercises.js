91927b7441cfe1ee1a9157455848c1d9729420e9
NathArcher <nathanielarcher@outlook.com>
2023-02-27 22:19:49 -0500

    class 6 exercises

---
+++ js/class6-exercises.js
@@ -0,0 +1,10 @@
+let score1 = [10,12,45,23,34];
+let score2 = [10,13,44,14,45];
+
+let newArr = []
+for (let i=0;i<score1.length; i++) {
+newArr[i]= score1[i]+score2[i]
+console.log (newArr[i]);
+}
+
+spliceArr = arr.splice (4,0,66,77);
