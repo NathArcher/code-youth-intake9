e8e5c277e1150c088da9afaa55f04fddd7a9cb60
NathArcher <nathanielarcher@outlook.com>
2023-03-03 22:22:10 -0500

    formatting/last questions

--- js/assignmentQuestions.js
+++ js/assignmentQuestions.js
@@ -71,14 +71,18 @@ countStringVowels(string);
 //   start, end, or step being undefined
 //   start being greater than end
 // step being 0, or negative
+//create a function with called range that will include 3 number parameters called start,end and step 
 function range (start,end,step){
+//if start, end or step are underfeined return a empty array
   if (start === undefined || end === undefined || step === undefined) {
     return [];
 }
+//if step is less than or equal to zero or start is greater than end return empty array
 if (step <= 0 || start > end ) {
 return [];
 }
 let result = [];
+//for loop which makes the array start with first number of the range, end after the second number that has been defined appears and increases by the third number
 for (let i = start; i<=end; i+=step){
   result.push(i);
 }
@@ -92,101 +96,105 @@ let codeOutputRange3 =  range (-5,2,3)
 console.log (codeOutputRange3)
 
 // Given an array of objects representing TV Shows, we want to create new arrays:
+
+const tvShows = [
+
+    {
+  
+      title: 'True Detective',
+  
+      publishingYear: 2014,
+  
+      rating: 9,
+  
+      watched: false,
+  
+    },
+  
+    {
+  
+      title: 'The Boys',
+  
+      publishingYear: 2019,
+  
+      rating: 8.7,
+  
+      watched: true,
+  
+    },
+  
+    {
+  
+      title: 'Game of Thrones',
+  
+      publishingYear: 2011,
+  
+      rating: 9.3,
+  
+      watched: true,
+  
+    },
+  
+    {
+  
+      title: 'Mr. Robot',
+  
+      publishingYear: 2015,
+  
+      rating: 8.5,
+  
+      watched: false,
+  
+    },
+  
+    {
+  
+      title: 'Chernobyl',
+  
+      publishingYear: 2019,
+  
+      rating: 9.4,
+  
+      watched: true,
+  
+    },
+  
+    {
+  
+      title: 'Money Heist',
+  
+      publishingYear: 2017,
+  
+      rating: 8.3,
+  
+      watched: true,
+  
+    },
+  
+    {
+  
+      title: 'Dark',
+  
+      publishingYear: 2017,
+  
+      rating: 8.8,
+  
+      watched: false,
+  
+    },
+  
+  ];
 // 1. One including only the titles of the shows
+  let titles = tvShows.map( show => show.title)
+  console.log (titles);
 // 2. One including objects that include only the title and the rating properties
+  let titlesAndRatings = tvShows.map (show => ({title:show.title, rating:show.rating}))
+  console.log (titlesAndRatings)
 // 3. One including only the titles of the shows, but:
-//   If the rating is greater or equal to 9, we put the title in upper case.
-//   Else, we make the title lower case.
-// (hint:You can create these 3 arrays using the map method)
+//  If the rating is greater or equal to 9, we put the title in upper case.
+//  Else, we make the title lower case.
+//  (hint:You can create these 3 arrays using the map method)
 // 4. One including only the TV Shows that were watched ({ watched: true }) filter
 // 5. One including only the TV Shows that have a rating greater or equal to 9 filter
 // 6. One including only the TV Shows that were first published before 2015 filter
 // (hint:You can create these 3 arrays using the filter method)
-
-// Data:const tvShows = [
-
-//     {
-  
-//       title: 'True Detective',
-  
-//       publishingYear: 2014,
-  
-//       rating: 9,
-  
-//       watched: false,
-  
-//     },
-  
-//     {
-  
-//       title: 'The Boys',
-  
-//       publishingYear: 2019,
-  
-//       rating: 8.7,
-  
-//       watched: true,
-  
-//     },
-  
-//     {
-  
-//       title: 'Game of Thrones',
-  
-//       publishingYear: 2011,
-  
-//       rating: 9.3,
-  
-//       watched: true,
-  
-//     },
-  
-//     {
-  
-//       title: 'Mr. Robot',
-  
-//       publishingYear: 2015,
-  
-//       rating: 8.5,
-  
-//       watched: false,
-  
-//     },
-  
-//     {
-  
-//       title: 'Chernobyl',
-  
-//       publishingYear: 2019,
-  
-//       rating: 9.4,
-  
-//       watched: true,
-  
-//     },
-  
-//     {
-  
-//       title: 'Money Heist',
-  
-//       publishingYear: 2017,
-  
-//       rating: 8.3,
-  
-//       watched: true,
-  
-//     },
-  
-//     {
-  
-//       title: 'Dark',
-  
-//       publishingYear: 2017,
-  
-//       rating: 8.8,
-  
-//       watched: false,
-  
-//     },
-  
-//   ];
