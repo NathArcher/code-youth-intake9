6cd1b7e0a6ee9498fc2bc15d8d47f2a82f9b0212
NathArcher <nathanielarcher@outlook.com>
2023-03-30 16:23:36 -0400

    Create users.js

---
+++ m1_fundamentals/Express/expressServer/users.js
@@ -0,0 +1,21 @@
+const users = [ 
+{
+    id:1,
+    name:"Alex",
+ },
+{
+    id:2,
+    name:"Sam",
+},
+{
+    id:3,
+    name:"Lucas",
+},
+
+{
+    id:4,
+    name:"Brian",
+}
+]
+
+module.exports = users
