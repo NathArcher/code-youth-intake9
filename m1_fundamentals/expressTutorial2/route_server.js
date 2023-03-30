6ee826b71bb39c60d94f183c36f140f6502ca860
NathArcher <nathanielarcher@outlook.com>
2023-03-30 16:24:00 -0400

    Delete route_server.js

--- m1_fundamentals/expressTutorial2/route_server.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express')
-const app = express()
-const userRoutes = require ('./routes/usersRoutes')
-const adminRoutes = require ('./routes/adminRoutes')
-
-
-app.use ('/user', userRoutes)
-app.use ('/admin', adminRoutes)
-app.listen (8080, ()=> console.log ("Server is running"))
